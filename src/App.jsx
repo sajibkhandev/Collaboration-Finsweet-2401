d16a44b1a544e15026b47db907c27aca66798f63
mohameddacar <mohameddacarmohumed@gmail.com>
2025-10-17 14:18:35 +0300

    dacar changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-500'>Mohamed Dacar Changes</div>
+    <div className='bg-red-500'>
+      <h1>Mohamed Dacar Changes</h1>
+      <p>Hello</p>
+    </div>
   )
 }
 
