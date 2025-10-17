f16b21ae19e0e4c37c3e163500f7cdd6a25c1d0f
sajibkhandev <sajibkhan.mern@gmail.com>
2025-10-17 19:18:28 +0600

    add component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Container from '../components/Container'
+import Flex from '../components/Flex'
+import Image from '../components/Image'
+import Sajib from '../assets/Youtube Thambnail (11).png'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+      <Flex className='justify-center'>
+       <h1>Wel</h1>
+       <h1>come!</h1>
+      </Flex>
+    </Container>
   )
 }
 
